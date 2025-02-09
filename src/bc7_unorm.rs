ede0658ec4f86a515704dd5c1ba8a7af74a86a02
adamthedash <adamthedash2@gmail.com>
2025-02-09 01:55:20 +0000

    More modes, also reverse bit order

--- src/bc7_unorm.rs
+++ src/bc7_unorm.rs
@@ -1,4 +1,4 @@
-use bitvec::{field::BitField, order::Msb0, slice::BitSlice};
+use bitvec::{field::BitField, order::Lsb0, slice::BitSlice};
 
 /// https://learn.microsoft.com/en-us/windows/win32/direct3d11/bc7-format-mode-reference
 /// https://github.com/microsoft/DirectXTex/blob/main/DirectXTex/BC6HBC7.cpp
@@ -207,7 +207,7 @@ const FIXUP_TABLE: [[usize; 3]; 64] = [
     [0, 3, 8],
 ];
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct RGB {
     pub r: u8,
     pub g: u8,
@@ -238,7 +238,7 @@ fn interpolate_u8(min: u8, max: u8, t: u8) -> u8 {
     low + ((high - low) as u16 * t as u16 / 255) as u8
 }
 
-pub fn decode_block_mode_0(block: &BitSlice<u8, Msb0>) -> [RGB; 16] {
+pub fn decode_block_mode_0(block: &BitSlice<u8, Lsb0>) -> [RGB; 16] {
     assert_eq!(block.len(), 128);
 
     let mode = block.first_one().expect("No valid BC7 mode found");
@@ -330,7 +330,7 @@ pub fn decode_block_mode_0(block: &BitSlice<u8, Msb0>) -> [RGB; 16] {
     pixels
 }
 
-pub fn decode_block_mode_1(block: &BitSlice<u8, Msb0>) -> [RGB; 16] {
+pub fn decode_block_mode_1(block: &BitSlice<u8, Lsb0>) -> [RGB; 16] {
     assert_eq!(block.len(), 128);
 
     let mode = block.first_one().expect("No valid BC7 mode found");
@@ -420,4 +420,283 @@ pub fn decode_block_mode_1(block: &BitSlice<u8, Msb0>) -> [RGB; 16] {
     //println!("Decoded: {:?}", pixels);
 
     pixels
+}
+
+fn print_bits(bits: &BitSlice<u8, Lsb0>) {
+    let out = bits
+        .iter()
+        .map(|x| if *x { 'X' } else { '-' })
+        .collect::<String>();
+
+    println!("{}", out);
+}
+
+pub fn decode_block_mode_2(block: &BitSlice<u8, Lsb0>) -> [RGB; 16] {
+    assert_eq!(block.len(), 128);
+
+    let mode = block.first_one().expect("No valid BC7 mode found");
+    assert_eq!(mode, 2);
+
+    // =========================== Constants for this mode
+    const RGB_BITS: usize = 5;
+    const PARTITION_BITS: usize = 6;
+    const RGB_INTERVALS: usize = 3;
+    const INDEX_BITS: usize = 2;
+
+    const PARTITION_OFFSET: usize = 3;
+    const RGBS_OFFSET: usize = PARTITION_OFFSET + PARTITION_BITS;
+    const INDICES_OFFSET: usize = RGBS_OFFSET + RGB_INTERVALS * 2 * 3 * RGB_BITS;
+    println!("{} {}", RGBS_OFFSET, INDICES_OFFSET);
+
+    print_bits(&block[..PARTITION_OFFSET]);
+    print_bits(&block[PARTITION_OFFSET..RGBS_OFFSET]);
+    print_bits(&block[RGBS_OFFSET..INDICES_OFFSET]);
+    print_bits(&block[INDICES_OFFSET..]);
+
+    // =========================== Step 1: Go from raw bits to raw native dtypes
+
+    // Table selection
+    let partition = block[PARTITION_OFFSET..PARTITION_OFFSET + PARTITION_BITS].load::<usize>();
+    let partition_table = PARTITION_TABLE_3[partition];
+    println!("partition: {} {:?}", partition, partition_table);
+    let fixup_table = FIXUP_TABLE[partition];
+    println!("fixup {:?}", fixup_table);
+
+    // Rx6 Gx6 Bx6
+    let rgbs = block[RGBS_OFFSET..INDICES_OFFSET]
+        .chunks_exact(RGB_BITS)
+        .map(|c| c.load::<u8>())
+        // Shift from 5 bit to 8 bit range
+        .map(|rgb| rgb << (8 - RGB_BITS))
+        .collect::<Vec<_>>();
+    println!("rgbs: {:?}", rgbs);
+
+    // Indices into intervals
+    let mut indices = [0; 16];
+    let index_block = &block[INDICES_OFFSET..];
+    let mut offset = 0;
+    indices.iter_mut().enumerate().for_each(|(i, x)| {
+        let width = if fixup_table.contains(&i) {
+            INDEX_BITS - 1
+        } else {
+            INDEX_BITS
+        };
+        *x = index_block[offset..offset + width].load::<u8>();
+        offset += width;
+    });
+    println!("indices: {:?}", indices);
+
+    // ========================= Step 2: Prepare RGB lookup structs
+    let rgb_intervals: [RGBInterval; RGB_INTERVALS] = (0..2 * RGB_INTERVALS)
+        .map(|i| RGB {
+            r: rgbs[i],
+            g: rgbs[i + 2 * RGB_INTERVALS],
+            b: rgbs[i + 2 * 2 * RGB_INTERVALS],
+        })
+        .array_chunks::<2>()
+        .map(|[min, max]| RGBInterval { min, max })
+        .collect::<Vec<_>>()
+        // No way to directly collect into an array :(
+        .try_into()
+        .unwrap();
+
+    println!("parsed rgbs: {:?}", rgb_intervals);
+
+    // ======================== Step 3: Lerp em!
+    let pixels = partition_table
+        .into_iter()
+        .zip(indices)
+        .map(|(table, index)| rgb_intervals[table].lerp(index))
+        .collect::<Vec<RGB>>()
+        // No way to directly collect into an array :(
+        .try_into()
+        .unwrap();
+    println!("Decoded: {:?}", pixels);
+
+    pixels
+}
+
+pub fn decode_block_mode_3(block: &BitSlice<u8, Lsb0>) -> [RGB; 16] {
+    assert_eq!(block.len(), 128);
+
+    let mode = block.first_one().expect("No valid BC7 mode found");
+    assert_eq!(mode, 3);
+
+    // =========================== Constants for this mode
+    const RGB_BITS: usize = 7;
+    const P_BITS: usize = 1;
+    const PARTITION_BITS: usize = 6;
+    const RGB_INTERVALS: usize = 2;
+    const INDEX_BITS: usize = 2;
+
+    const PARTITION_OFFSET: usize = 3;
+    const RGBS_OFFSET: usize = PARTITION_OFFSET + PARTITION_BITS;
+    const PS_OFFSET: usize = RGBS_OFFSET + RGB_INTERVALS * 2 * 3 * RGB_BITS;
+    const INDICES_OFFSET: usize = PS_OFFSET + RGB_INTERVALS * 2 * P_BITS;
+    //println!("{} {} {}", RGBS_OFFSET, PS_OFFSET, INDICES_OFFSET);
+
+    // =========================== Step 1: Go from raw bits to raw native dtypes
+
+    // Table selection
+    let partition = block[PARTITION_OFFSET..PARTITION_OFFSET + PARTITION_BITS].load::<usize>();
+    let partition_table = PARTITION_TABLE_2[partition];
+    //println!("partition: {} {:?}", partition, partition_table);
+    let fixup_table = FIXUP_TABLE[partition];
+    //println!("fixup {:?}", fixup_table);
+
+    // Rx6 Gx6 Bx6
+    let ps = &block[PS_OFFSET..INDICES_OFFSET];
+    let rgbs = block[RGBS_OFFSET..PS_OFFSET]
+        .chunks_exact(RGB_BITS)
+        .map(|c| c.load::<u8>())
+        .enumerate()
+        // Shift by common offset
+        .map(|(i, rgb)| {
+            if ps[i % (2 * RGB_INTERVALS)] {
+                rgb << P_BITS
+            } else {
+                rgb
+            }
+        })
+        // Shift from 5 bit to 8 bit range
+        .map(|rgb| rgb << (8 - RGB_BITS - P_BITS))
+        .collect::<Vec<_>>();
+    //println!("rgbs: {:?}", rgbs);
+
+    // Indices into intervals
+    let mut indices = [0; 16];
+    let index_block = &block[INDICES_OFFSET..];
+    let mut offset = 0;
+    indices.iter_mut().enumerate().for_each(|(i, x)| {
+        let width = if fixup_table.contains(&i) {
+            INDEX_BITS - 1
+        } else {
+            INDEX_BITS
+        };
+        *x = index_block[offset..offset + width].load::<u8>();
+        offset += width;
+    });
+    //println!("indices: {:?}", indices);
+
+    // ========================= Step 2: Prepare RGB lookup structs
+    let rgb_intervals: [RGBInterval; RGB_INTERVALS] = (0..2 * RGB_INTERVALS)
+        .map(|i| RGB {
+            r: rgbs[i],
+            g: rgbs[i + 2 * RGB_INTERVALS],
+            b: rgbs[i + 2 * 2 * RGB_INTERVALS],
+        })
+        .array_chunks::<2>()
+        .map(|[min, max]| RGBInterval { min, max })
+        .collect::<Vec<_>>()
+        // No way to directly collect into an array :(
+        .try_into()
+        .unwrap();
+
+    //println!("parsed rgbs: {:?}", rgb_intervals);
+
+    // ======================== Step 3: Lerp em!
+    let pixels = partition_table
+        .into_iter()
+        .zip(indices)
+        .map(|(table, index)| rgb_intervals[table].lerp(index))
+        .collect::<Vec<RGB>>()
+        // No way to directly collect into an array :(
+        .try_into()
+        .unwrap();
+    //println!("Decoded: {:?}", pixels);
+
+    pixels
+}
+
+pub fn decode_block_mode_4(block: &BitSlice<u8, Lsb0>) -> [RGB; 16] {
+    assert_eq!(block.len(), 128);
+
+    let mode = block.first_one().expect("No valid BC7 mode found");
+    assert_eq!(mode, 4);
+
+    // =========================== Constants for this mode
+    const RGB_BITS: usize = 5;
+    const A_BITS: usize = 6;
+    const RGB_INTERVALS: usize = 1;
+
+    const RGBS_OFFSET: usize = 4 + 2;
+    const AS_OFFSET: usize = RGBS_OFFSET + RGB_INTERVALS * 2 * 3 * RGB_BITS;
+    const INDICES_OFFSET: usize = AS_OFFSET + RGB_INTERVALS * 2 * A_BITS;
+
+    // =========================== Step 1: Go from raw bits to raw native dtypes
+
+    let rotation = block[4..6].load::<u8>();
+    println!("rotation: {}", rotation);
+
+    // Rx6 Gx6 Bx6
+    let rgbs = block[RGBS_OFFSET..AS_OFFSET]
+        .chunks_exact(RGB_BITS)
+        .map(|c| c.load::<u8>())
+        // Shift from 5 bit to 8 bit range
+        .map(|rgb| rgb << (8 - RGB_BITS))
+        .collect::<Vec<_>>();
+    println!("rgbs: {:?}", rgbs);
+
+    let alphas = block[AS_OFFSET..INDICES_OFFSET]
+        .chunks_exact(A_BITS)
+        .map(|c| c.load::<u8>())
+        // Shift from 5 bit to 8 bit range
+        .map(|rgb| rgb << (8 - A_BITS))
+        .collect::<Vec<_>>();
+    println!("alphas: {:?}", alphas);
+
+    // Indices into intervals
+    let index_bits = if block[6] { 3 } else { 2 };
+    let indices = block[INDICES_OFFSET..]
+        .chunks_exact(index_bits)
+        .take(16)
+        .map(|chunk| chunk.load::<u8>())
+        .map(|rgb| rgb << (8 - index_bits))
+        .collect::<Vec<_>>();
+
+    println!("indices: {:?}", indices);
+
+    // ========================= Step 2: Prepare RGB lookup structs
+    let rgb_intervals: [RGBInterval; RGB_INTERVALS] = (0..2 * RGB_INTERVALS)
+        .map(|i| RGB {
+            r: rgbs[i],
+            g: rgbs[i + 2 * RGB_INTERVALS],
+            b: rgbs[i + 2 * 2 * RGB_INTERVALS],
+        })
+        .array_chunks::<2>()
+        .map(|[min, max]| RGBInterval { min, max })
+        .collect::<Vec<_>>()
+        // No way to directly collect into an array :(
+        .try_into()
+        .unwrap();
+
+    //println!("parsed rgbs: {:?}", rgb_intervals);
+
+    // ======================== Step 3: Lerp em!
+    let pixels = indices
+        .into_iter()
+        .map(|index| rgb_intervals[0].lerp(index))
+        .collect::<Vec<_>>()
+        // No way to directly collect into an array :(
+        .try_into()
+        .unwrap();
+    //println!("Decoded: {:?}", pixels);
+
+    pixels
+}
+
+#[cfg(test)]
+mod tests {
+    use super::interpolate_u8;
+
+    #[test]
+    fn test_interp() {
+        assert_eq!(interpolate_u8(0, 255, 0), 0);
+        assert_eq!(interpolate_u8(0, 255, 255), 255);
+        assert_eq!(interpolate_u8(0, 128, 255), 128);
+        assert_eq!(interpolate_u8(32, 64, 128), 48);
+    }
+
+    #[test]
+    fn test_mode0() {}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,16 @@ pub mod bc7_unorm;
 mod tests {
     use std::{collections::HashMap, fs::File, io::BufReader, path::Path};
 
-    use bitvec::{order::Msb0, view::BitView};
+    use bitvec::{
+        order::{Lsb0, Msb0},
+        view::BitView,
+    };
     use ddsfile::{Caps, DataFormat, Dds, FourCC};
 
-    use crate::bc7_unorm::{decode_block_mode_0, decode_block_mode_1};
+    use crate::bc7_unorm::{
+        decode_block_mode_0, decode_block_mode_1, decode_block_mode_2, decode_block_mode_3,
+        decode_block_mode_4, RGB,
+    };
 
     #[test]
     fn test_load_art() {
@@ -32,6 +38,8 @@ mod tests {
         );
 
         println!("{:?}, {:?}", dds.get_d3d_format(), dds.get_dxgi_format());
+        let image_height = dds.get_height() as usize;
+        let image_width = dds.get_width() as usize;
         println!(
             "{} {} {} {:?}",
             dds.get_height(),
@@ -42,6 +50,8 @@ mod tests {
         println!("{:?}", dds.get_num_mipmap_levels());
 
         let fm = dds.get_dxgi_format().unwrap();
+        let block_size = fm.get_block_size().unwrap() as usize;
+        let block_height = fm.get_pitch_height() as usize;
         println!(
             "{:?}, {:?} {:?}",
             fm.get_block_size(),
@@ -54,8 +64,8 @@ mod tests {
             println!("{} {}", i, data.len());
 
             let blocks = data
-                .chunks_exact(16)
-                .map(|block| block.view_bits::<Msb0>())
+                .chunks_exact(block_size)
+                .map(|block| block.view_bits::<Lsb0>())
                 .collect::<Vec<_>>();
             println!("num blocks: {}", blocks.len());
 
@@ -71,7 +81,9 @@ mod tests {
             );
             println!("modes: {:?}", modes);
 
-            //decode_block_mode_0(&blocks[0]);
+            //decode_block_mode_4(blocks[0]);
+            //decode_block_mode_2(blocks[60]);
+            //panic!();
 
             let decoded = blocks
                 .into_iter()
@@ -79,12 +91,39 @@ mod tests {
                     let decoded = match b.first_one().unwrap() {
                         0 => decode_block_mode_0(b),
                         1 => decode_block_mode_1(b),
-                        _ => return None,
+                        2 => decode_block_mode_2(b),
+                        3 => decode_block_mode_3(b),
+                        4 => decode_block_mode_4(b),
+                        _ => [RGB { r: 0, g: 0, b: 0 }; 16],
                     };
                     Some(decoded)
                 })
                 .collect::<Vec<_>>();
             println!("done {}", decoded.len());
+
+            // Combine blocks into single image
+            let num_blocks_y = image_height / block_height;
+            let num_blocks_x = image_width / block_height;
+            let num_blocks = num_blocks_y * num_blocks_x;
+            let biggest_image = &decoded[..num_blocks];
+
+            let mut img = image::RgbImage::new(image_width as u32, image_height as u32);
+            biggest_image.iter().enumerate().for_each(|(i, block)| {
+                let block_x = i % num_blocks_x;
+                let block_y = i / num_blocks_x;
+
+                block.iter().enumerate().for_each(|(j, pixel)| {
+                    let x_inner = j % 4;
+                    let y_inner = j / 4;
+                    let y = (block_y * block_height) + y_inner;
+                    let x = (block_x * block_height) + x_inner;
+                    //println!("{} {} {} {}", i, j, y, x);
+
+                    img.put_pixel(x as u32, y as u32, image::Rgb([pixel.r, pixel.g, pixel.b]));
+                });
+            });
+
+            img.save("test.png").unwrap();
         }
     }
 }
